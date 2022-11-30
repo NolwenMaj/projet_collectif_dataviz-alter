47820e6e878cf388b3c03f464fd8dceaf8510a75
HelderF2821 <helder.ferreira2821@gmail.com>
2022-11-30 15:46:21 +0100

    API News

--- ComicSansMsPowaahh/script.js
+++ ComicSansMsPowaahh/script.js
@@ -870,14 +870,17 @@ getAJoke.onclick = () => {
 
 // variables météos
 
-  let urlMeteo = "https://api.open-meteo.com/v1/forecast?latitude=48.85&longitude=2.35&hourly=temperature_2m&current_weather=true&daily=temperature_2m_min,temperature_2m_max&timezone=auto";
-  const now = /* 14 */ new Date().getHours("fr-FR")  
-  const intemperies = document.getElementById("intemperies");
-  const nowParsed = parseInt(now);
-  const colorDay = "#acbefe";
-  const colorNight = "#293254";
-  const linearLeverSoleil ="linear-gradient(180deg, #080060 0%, #89A3FF 44.79%, #FAE082)";
-  const linearCoucherSoleil ="linear-gradient(180deg, #0E0036 0%, #293254 13.02%, #ACBEFE 65.1%, rgba(198, 172, 254, 0.62) 81.25%, rgba(255, 166, 230, 0.55) 85.94%, rgba(250, 224, 130, 0.85) 96.35%)";
+let urlMeteo =
+  "https://api.open-meteo.com/v1/forecast?latitude=48.85&longitude=2.35&hourly=temperature_2m&current_weather=true&daily=temperature_2m_min,temperature_2m_max&timezone=auto";
+const now = 14; // new Date().getHours("fr-FR")
+const intemperies = document.getElementById("intemperies");
+const nowParsed = parseInt(now);
+const colorDay = "#acbefe";
+const colorNight = "#293254";
+const linearLeverSoleil =
+  "linear-gradient(180deg, #080060 0%, #89A3FF 44.79%, #FAE082)";
+const linearCoucherSoleil =
+  "linear-gradient(180deg, #0E0036 0%, #293254 13.02%, #ACBEFE 65.1%, rgba(198, 172, 254, 0.62) 81.25%, rgba(255, 166, 230, 0.55) 85.94%, rgba(250, 224, 130, 0.85) 96.35%)";
 
 // gestion du fond en fonction de l'heure de la journée
 
@@ -916,21 +919,15 @@ function asynchrone() {
 
       // affichage des pictos et fonds intempéries
       const symbole = document.createElement("img");
-      const codeWeather = 45 ; // let codeWeather = data.current_weather.weathercode // variable qui fait changer le picto de la méteo
-         
+      const codeWeather = 2; // let codeWeather = data.current_weather.weathercode // variable qui fait changer le picto de la méteo
+
       if (nowParsed > 8 && nowParsed < 17) {
         switch (codeWeather) {
           case 0: // sunny
             symbole.src = "mm_api_symbols/wsymbol_0001_sunny.png";
-            intemperies.style.backgroundImage = "url('soleil.png')";
-            intemperies.style.backgroundRepeat = "no-repeat";
-            intemperies.style.backgroundSize ="cover"
             break;
           case 1: // mainly sunny
             symbole.src = "mm_api_symbols/wsymbol_0002_sunny_intervals.png";
-            intemperies.style.backgroundImage = "url('soleil.png')";
-            intemperies.style.backgroundRepeat = "no-repeat";
-            intemperies.style.backgroundSize ="cover"
             break;
           case 2: // white cloud
             symbole.src = "mm_api_symbols/wsymbol_0003_white_cloud.png";
@@ -947,9 +944,6 @@ function asynchrone() {
             break;
           case 45:
             symbole.src = "mm_api_symbols/wsymbol_0006_mist.png";
-            intemperies.style.backgroundImage = "url('brouillardtransparent.png')";
-            intemperies.style.backgroundRepeat = "no-repeat";
-            intemperies.style.backgroundSize ="10%"
           case 48:
             symbole.src = "mm_api_symbols/wsymbol_0007_fog.png";
             break;
@@ -1123,191 +1117,15 @@ asynchrone();
 
 /* ---------------------- API NEWS -------- */
 
-const data = {
-  status: "success",
-  totalResults: 132,
-  results: [
-    {
-      title:
-        "Mercredi : la famille Addams est loin d’avoir dit son dernier mot",
-      link: "https://www.journaldugeek.com/2022/11/28/mercredi-la-famille-addams-est-loin-davoir-dit-son-dernier-mot/",
-      keywords: [Array],
-      creator: [Array],
-      video_url: null,
-      description:
-        "Les créateurs de Mercredi en ont encore beaucoup sous la main. Les choses vont-elles évoluer dans le bon sens pour l'adolescente ? Mercredi : la famille Addams est loin d’avoir dit son dernier mot",
-      content: null,
-      pubDate: "2022-11-28 13:30:57",
-      image_url: null,
-      source_id: "journaldugeek",
-      country: [Array],
-      category: [Array],
-      language: "french",
-    },
-    {
-      title:
-        "L’excellente souris Logitech MX Master 2S voit son prix divisé par 2 (-52%) !",
-      link: "https://www.journaldugeek.com/bon-plan/lexcellente-souris-logitech-mx-master-2s-voit-son-prix-divise-par-2-52/",
-      keywords: [Array],
-      creator: [Array],
-      video_url: null,
-      description:
-        'Profitez de 52% de réduction sur la souris Logitech MX Master 2S, un "must have" à ce prix-là ! L’excellente souris Logitech MX Master 2S voit son prix divisé par 2 (-52%) !',
-      content: null,
-      pubDate: "2022-11-28 13:17:05",
-      image_url: null,
-      source_id: "journaldugeek",
-      country: [Array],
-      category: [Array],
-      language: "french",
-    },
-    {
-      title:
-        "Les astronautes de l’ISS sont confinés à cause de leurs combinaisons",
-      link: "https://www.journaldugeek.com/2022/11/28/les-astronautes-de-liss-sont-confines-a-cause-de-leurs-combinaisons/",
-      keywords: [Array],
-      creator: [Array],
-      video_url: null,
-      description:
-        "Après les couacs de la NASA au début de l'année, c'est la Russie et Roscomos qui ont aujourd'hui des problèmes avec leurs combinaisons. Les astronautes de l’ISS sont confinés à cause de leurs combinaisons",
-      content: null,
-      pubDate: "2022-11-28 13:00:52",
-      image_url: null,
-      source_id: "journaldugeek",
-      country: [Array],
-      category: [Array],
-      language: "french",
-    },
-    {
-      title:
-        "Apple explique pourquoi les AirPods Pro ne supportent pas le LossLess",
-      link: "https://www.journaldugeek.com/2022/11/28/apple-explique-pourquoi-les-airpods-pro-ne-supportent-pas-le-lossless/",
-      keywords: [Array],
-      creator: [Array],
-      video_url: null,
-      description: `Apple n'a pas apporté sa technologie "LossLess" aux AirPods Pro 2. Mais selon un ingénieur de la Pomme, elle a une bonne raison. Apple explique pourquoi les AirPods Pro ne supportent pas le LossLess`,
-      content: null,
-      pubDate: "2022-11-28 12:30:28",
-      image_url: null,
-      source_id: "journaldugeek",
-      country: [Array],
-      category: [Array],
-      language: "french",
-    },
-    {
-      title: "Epic Games Store : le calendrier de l’avent bientôt de retour ?",
-      link: "https://www.journaldugeek.com/2022/11/28/epic-games-store-le-calendrier-de-lavent-bientot-de-retour/",
-      keywords: [Array],
-      creator: [Array],
-      video_url: null,
-      description:
-        "Cette année à Noël, les joueurs Pc vont être une nouvelle fois gâtés par Epic Games. Epic Games Store : le calendrier de l’avent bientôt de retour ?",
-      content: null,
-      pubDate: "2022-11-28 12:00:56",
-      image_url: null,
-      source_id: "journaldugeek",
-      country: [Array],
-      category: [Array],
-      language: "french",
-    },
-    {
-      title: "China's Covid protests lead to uncertainty on stock markets",
-      link: "https://www.france24.com/en/tv-shows/business-daily/20221128-china-s-covid-protests-lead-to-uncertainty-on-stock-markets",
-      keywords: [Array],
-      creator: [Array],
-      video_url: null,
-      description:
-        "Nationwide protests against China's zero-Covid policy have left investors concerned about the outlook for the country's economy. On Monday morning, both the Shanghai Composite and Hang Seng in Hong Kong were posting losses. The price of oil was also affected, on the assumption that Chinese demand for crude will be weaker. Also in this edition, we take a look at record online sales during Black Friday.",
-      content:
-        "/ Shows / Business Daily China's Covid protests lead to uncertainty on stock markets Issued on: 28/11/2022 - 12:46 Audio 05:04 05:04 BUSINESS DAILY © FRANCE 24 By: Charles PELLEGRIN Follow Nationwide protests against China's zero-Covid policy have left investors concerned about the outlook for the country's economy. On Monday morning, both the Shanghai Composite and Hang Seng in Hong Kong were posting losses. The price of oil was also affected, on the assumption that Chinese demand for crude will be weaker. Also in this edition, we take a look at record online sales during Black Friday. China Coronavirus (Covid-19) markets protest Related content In the press #WhitePaperRevolution: Chinese protesters seek to bypass censorship China attempts to curb, censor rare nationwide protests over Covid lockdowns Protests in Shanghai as anger mounts over China's 'zero-Covid' lockdown policy Previous shows 25/11/2022 Black Friday sales remain popular with French consumers 24/11/2022 EU energy ministers hold tense talks on Russian natural gas price cap 21/11/2022 With $220 billion price tag, Qatar hosts most expensive World Cup in history 17/11/2022 Elon Musk defends his $56 billion Tesla pay package",
-      pubDate: "2022-11-28 11:46:17",
-      image_url:
-        "https://s.france24.com/media/display/3de3012e-6efc-11ed-9936-005056a90284/w:1024/p:16x9/ECO%20CHINA%20PROTESTS%2006H%20-ROBERTSON%20Georgina-.jpg",
-      source_id: "france24",
-      country: [Array],
-      category: [Array],
-      language: "english",
-    },
-    {
-      title:
-        "Les robots policiers pourraient bientôt avoir leur permis de tuer",
-      link: "https://www.journaldugeek.com/2022/11/28/les-robots-policiers-pourraient-bientot-avoir-leur-permis-de-tuer/",
-      keywords: [Array],
-      creator: [Array],
-      video_url: null,
-      description:
-        "Les robots tueurs dans la police vont bientôt devenir une réalité. Les robots policiers pourraient bientôt avoir leur permis de tuer",
-      content: null,
-      pubDate: "2022-11-28 11:30:33",
-      image_url: null,
-      source_id: "journaldugeek",
-      country: [Array],
-      category: [Array],
-      language: "french",
-    },
-    {
-      title: "Vous ne reverrez plus jamais la Nvidia Shield TV à ce prix 😍",
-      link: "https://www.journaldugeek.com/bon-plan/vous-ne-reverrez-plus-jamais-la-nvidia-shield-tv-a-ce-prix/",
-      keywords: [Array],
-      creator: [Array],
-      video_url: null,
-      description:
-        "Si vous êtes à la recherche d’une box TV sous Android fonctionnelle et efficace, cette promotion pourrait vous plaire. La Nvidia Shield TV est actuellement disponible à 109 euros pour Black Friday, du jamais vu ! Vous ne reverrez plus jamais la Nvidia Shield TV à ce prix 😍",
-      content: null,
-      pubDate: "2022-11-28 11:15:00",
-      image_url: null,
-      source_id: "journaldugeek",
-      country: [Array],
-      category: [Array],
-      language: "french",
-    },
-    {
-      title: "Métavers: un an après l’euphorie, le retour sur terre",
-      link: "https://www.lefigaro.fr/secteur/high-tech/metavers-un-an-apres-l-euphorie-le-retour-sur-terre-20221127",
-      keywords: [Array],
-      creator: null,
-      video_url: null,
-      description:
-        "ENQUÊTE - L’emballement trop précoce des marques fait place à la prudence, alors que le terme métavers, «galvaudé», est désormais rejeté par des acteurs de la réalité virtuelle et augmentée.",
-      content: null,
-      pubDate: "2022-11-28 10:18:02",
-      image_url: null,
-      source_id: "lefigaro",
-      country: [Array],
-      category: [Array],
-      language: "french",
-    },
-    {
-      title:
-        "Cyber Monday : dernier jour pour profiter des promos tech qui valent vraiment le coup !",
-      link: "https://www.numerama.com/tech/1192864-cyber-monday-dernier-jour-pour-profiter-des-promos-tech-qui-valent-vraiment-le-coup.html",
-      keywords: [Array],
-      creator: [Array],
-      video_url: null,
-      description:
-        "Place au Cyber Monday. Après une semaine rythmée par les réductions sur Amazon, Fnac et compagnies, et de promotions sur les grandes marques comme Apple ou Samsung, la période du Black Friday se cloture par le Cyber Monday. C'est en pratique le dernier jour pour profiter encore d'offres sur de nombreux poduits tech.",
-      content:
-        "Place au Cyber Monday. Après une semaine rythmée par les réductions sur Amazon, Fnac et compagnies, et de promotions sur les grandes marques comme Apple ou Samsung, la période du Black Friday se cloture par le Cyber Monday. C'est en pratique le dernier jour pour profiter encore d'offres sur de nombreux poduits tech.  [Lire la suite]Abonnez-vous aux newsletters Numerama pour recevoir l’essentiel de l’actualité https://www.numerama.com/newsletter/",
-      pubDate: "2022-11-28 09:20:33",
-      image_url: null,
-      source_id: "numerama",
-      country: [Array],
-      category: [Array],
-      language: "french",
-    },
-  ],
-  nextPage: 1,
-};
-
 // fetch sur l'appli de news
-/*const urlNews =
+const urlNews =
   "https://newsdata.io/api/1/news?apikey=pub_1395503c37b0848da13441770cfe5743622fd&country=fr&category=technology&language=fr,en";
 
 fetch(urlNews)
   .then((response) => response.json())
-  .then((data) => console.log(data));
-  .then(function (data) { ... INSERER ICI LA FONCTION FOR ... } 
-  */
+  // .then((data) => console.log(data))
+  // .then((data) => console.log(data.status))
+  .then(function (data) {
 
 for (var i = 0; i < 3; i++) {
   // pour prendre uniquements les trois derniers articles parus
@@ -1316,58 +1134,51 @@ for (var i = 0; i < 3; i++) {
   liensNews = data.results[i].link;
   imgsrc = data.results[i].image_url;
 
-  generate_table();
-}
-
-function generate_table() {
-  var body = document.getElementById("news"); // désigne l'endroit (body) où sera inséré le tableau
-  var tbl = document.createElement("table"); // crée un élément <table> et un élément tblBody, qui est le contenu du tableau
-  var tblBody = document.createElement("tbody");
-  var column = document.createElement("td"); // crée une colonne qui s'étend sur 3 lignes (rowSpan)
-  column.rowSpan = "3";
-
-  tbl.setAttribute("lien", liensNews);
-
-  img = document.createElement("IMG"); // création de l'élément image
-  if (data.results[i].image_url == null) {
-    img.src =
-      "https://soniseo.com/wp-content/uploads/2017/05/technology-news-sites.jpg";
-  } else {
-    img.src = data.results[i].image_url;
+  function generate_table() {
+    var body = document.getElementById("news"); // désigne l'endroit (body) où sera inséré le tableau
+    var tbl = document.createElement("table"); // crée un élément <table> et un élément tblBody, qui est le contenu du tableau
+    var tblBody = document.createElement("tbody");
+    var column = document.createElement("td"); // crée une colonne qui s'étend sur 3 lignes (rowSpan)
+    column.rowSpan = "3";
+  
+    tbl.setAttribute("lien", liensNews);
+  
+    img = document.createElement("IMG"); // création de l'élément image
+    if (data.results[i].image_url == null) {
+      img.src =
+        "https://soniseo.com/wp-content/uploads/2017/05/technology-news-sites.jpg";
+    } else {
+      img.src = data.results[i].image_url;
+    }
+    img.style.width = "100px";
+    img.style.height = "auto";
+    img.setAttribute("id", "imagesNews");
+  
+    column.appendChild(img); // ajout de l'image à l'élément parent colonne
+    tblBody.appendChild(column); // ajout de la colonne à l'élément parent "contenu du tableau" (tblBody)
+  
+    var row = document.createElement("tr"); // création d'une ligne et de son contenu (rowText)
+    var rowText = document.createTextNode(title);
+    row.appendChild(rowText); // ajout du contenu à l'élément parent ligne (row)
+    row.setAttribute("id", "titleArticle");
+  
+    row.onclick = () => {
+      // attribue le lien à cliquer à la news
+      window.open(tbl.getAttribute("lien"), "_blank");
+    };
+  
+    var row2 = document.createElement("tr"); // création d'une deuxième ligne et de son contenu
+    var row2text = document.createTextNode(description);
+  
+    row2.appendChild(row2text); // ajout du contenu à l'élément parent ligne2 (row2)
+    row2.setAttribute("id", "descriptionArticle"); // ajout d'un id pour la gestion du style
+  
+    row.append(row2); // ajout de la ligne 2 à l'élément parent ligne 1 (row)
+  
+    tblBody.appendChild(row); // ajout des lignes (élément parent row) au contenu du tableau (élément parent tblBody)
+    tbl.appendChild(tblBody); // ajout du contenu du tableau au tableau
+    body.appendChild(tbl); // ajout du tableau à l'élément HTML "news"
   }
-  img.style.width = "100px";
-  img.style.height = "auto";
-  img.setAttribute("id", "imagesNews");
+  generate_table();
 
-  column.appendChild(img); // ajout de l'image à l'élément parent colonne
-  tblBody.appendChild(column); // ajout de la colonne à l'élément parent "contenu du tableau" (tblBody)
-
-  var row = document.createElement("tr"); // création d'une ligne et de son contenu (rowText)
-  var rowText = document.createTextNode(title);
-  row.appendChild(rowText); // ajout du contenu à l'élément parent ligne (row)
-  row.setAttribute("id", "titleArticle");
-
-  row.onclick = () => {
-    // attribue le lien à cliquer à la news
-    window.open(tbl.getAttribute("lien"), "_blank");
-  };
-
-  var row2 = document.createElement("tr"); // création d'une deuxième ligne et de son contenu
-  var row2text = document.createTextNode(description);
-
-  row2.appendChild(row2text); // ajout du contenu à l'élément parent ligne2 (row2)
-  row2.setAttribute("id", "descriptionArticle"); // ajout d'un id pour la gestion du style
-
-  row.append(row2); // ajout de la ligne 2 à l'élément parent ligne 1 (row)
-
-  // code non utilisé : création d'une ligne 3 pour afficher le lien de la news
-  /* var row3 = document.createElement("tr");
-    var row3text = document.createTextNode(link.link(link));
-    row3.appendChild(row3text);
-    row3.setAttribute("border", "2");
-    row.append(row3);  */
-
-  tblBody.appendChild(row); // ajout des lignes (élément parent row) au contenu du tableau (élément parent tblBody)
-  tbl.appendChild(tblBody); // ajout du contenu du tableau au tableau
-  body.appendChild(tbl); // ajout du tableau à l'élément HTML "news"
-}
+}})
